b4f237d2b3b1a24a81c1e1d99aa727754cc9a4e5
Bhaskar Dhara <dharabhaskar@gmail.com>
2021-10-04 15:20:38 +0530

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/infocus/TimeApproval/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.infocus.TimeApproval.controller.MainView", {});
+});
